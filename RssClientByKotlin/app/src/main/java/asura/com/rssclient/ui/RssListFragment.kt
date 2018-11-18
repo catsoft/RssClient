267cb5951445fe2d90ea15b97651be34e1962132
asura <theworldtocollapse@mail.ru>
2018-11-18 21:06:51 +0500

    add navigate

--- RssClientByKotlin/app/src/main/java/asura/com/rssclient/ui/RssListFragment.kt
+++ RssClientByKotlin/app/src/main/java/asura/com/rssclient/ui/RssListFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import asura.com.rssclient.databinding.FragmentRssListBinding
+import asura.com.rssclient.R
 
 
 class RssListFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentRssListBinding.inflate(inflater, container, false)
+
+        binding.noItems.setOnClickListener {
+            it.findNavController().navigate(R.id.rss_detail_fragment)
+        }
 
         return binding.root
     }
