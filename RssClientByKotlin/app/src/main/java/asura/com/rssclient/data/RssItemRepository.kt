76aee501da2051068e77b00335da7c609a44db0a
Alexandr Vakhtin <a.vakhtin@skbkontur.ru>
2018-11-28 16:47:34 +0500

    rep

---
+++ RssClientByKotlin/app/src/main/java/asura/com/rssclient/data/RssItemRepository.kt
@@ -0,0 +1,26 @@
+package asura.com.rssclient.data
+
+import androidx.lifecycle.LiveData
+
+class RssItemRepository private constructor(private val rssItemDao: RssItemDao){
+
+    fun unsertItem(item : RssItem) = rssItemDao.insertRssItem(item)
+
+    fun deleteItem(item : RssItem) = rssItemDao.deleteRssItem(item)
+
+    fun getItems() : LiveData<List<RssItem>>
+    {
+        return rssItemDao.getRssItemsList()
+    }
+
+    companion object {
+
+        // For Singleton instantiation
+        @Volatile private var instance: RssItemRepository? = null
+
+        fun getInstance(gardenPlantingDao: RssItemDao) =
+            instance ?: synchronized(this) {
+                instance ?: RssItemRepository(gardenPlantingDao).also { instance = it }
+            }
+    }
+}
