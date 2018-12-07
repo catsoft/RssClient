3772941e8efc179392a9b09ab2cad66faf7410fa
Alexandr Vakhtin <a.vakhtin@skbkontur.ru>
2018-12-07 13:24:08 +0500

    xamarin init ios project

---
+++ RssClientByXamarin/iOS/App/Base/ViewController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UIKit;
+
+namespace iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
