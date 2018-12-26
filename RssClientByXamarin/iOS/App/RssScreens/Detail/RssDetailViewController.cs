ebc0eca3d0948a9276ee57aa52822987110e03e1
Alexandr Vakhtin <a.vakhtin@skbkontur.ru>
2018-12-26 21:33:02 +0500

    Сделал удаление Rss

--- RssClientByXamarin/iOS/App/RssScreens/Detail/RssDetailViewController.cs
+++ RssClientByXamarin/iOS/App/RssScreens/Detail/RssDetailViewController.cs
@@ -1,78 +1,84 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Database.Rss;
 using iOS.App.Base.Stated;
 using iOS.App.Base.Table;
 using iOS.App.RssScreens.Edit;
 using Realms;
+using Shared.App.Rss;
 using Shared.App.Rss.RssDatabase;
+using UIKit;
 using Xamarin;
 
 namespace iOS.App.RssScreens.Detail
 {
-	public class RssDetailViewController : BaseTableViewController<RssMessageViewCell, RssMessageModel, IEnumerable<RssMessageModel>>
-	{
-		private readonly RssModel _item;
-		private readonly RssMessagesRepository _rssMessagesRepository;
+    public class RssDetailViewController : BaseTableViewController<RssMessageViewCell, RssMessageModel, IEnumerable<RssMessageModel>>
+    {
+        private readonly RssModel _item;
+        private readonly RssMessagesRepository _rssMessagesRepository;
+        private readonly RssRepository _repository;
 
-		public RssDetailViewController(RssModel item)
-		{
-			_item = item;
+        public RssDetailViewController(RssModel item)
+        {
+            _item = item;
 
-			_rssMessagesRepository = RssMessagesRepository.Instance;
-		}
-
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-
-			if (NavigationItem != null)
-			{
-				NavigationItem.Title = _item.Name;
-
-				var editButton = new IQBarButtonItem();
-				editButton.Title = "Edit";
-				editButton.Clicked += (sender, args) =>
-				{
-					NavigationController?.PushViewController(new RssEditViewController(_item), true);
-				};
-
-				NavigationItem.RightBarButtonItem = editButton;
-			}
-
-			var items = _rssMessagesRepository.GetMessagesForRss(_item);
-
-			Source.SetList(items);
-
-            DataSetChanges();
-
-            _item.PropertyChanged += (sender, args) =>
-            {
-                var newItems = _rssMessagesRepository.GetMessagesForRss(_item);
-                Source.SetList(newItems);
-
-                DataSetChanges();
-            };
-
-            _item.RssMessageModels.SubscribeForNotifications((sender, changes, error) =>
-            {
-                var newItems = _rssMessagesRepository.GetMessagesForRss(_item);
-                Source.SetList(newItems);
-
-                DataSetChanges();
-            });
+            _rssMessagesRepository = RssMessagesRepository.Instance;
+            _repository = RssRepository.Instance;
         }
 
-        private void DataSetChanges()
+        public override void ViewDidLoad()
         {
-            TableView.ReloadData();
+            base.ViewDidLoad();
 
-            if (Source.ItemsCount > 0)
+            if (NavigationItem != null)
             {
-                StatedDecorator.SetNormal(new NormalData());
+                NavigationItem.Title = _item.Name;
+
+                var editButton = new IQBarButtonItem {Title = "Edit"};
+                editButton.Clicked += (sender, args) => { NavigationController?.PushViewController(new RssEditViewController(_item), true); };
+
+                var deleteButton = new IQBarButtonItem {Title = "Delete"};
+                deleteButton.Clicked += (sender, args) =>
+                {
+                    _repository.Remove(_item);
+                    NavigationController?.PopViewController(true);
+                };
+
+                NavigationItem.RightBarButtonItems = new UIBarButtonItem[] {editButton, deleteButton};
             }
-            else
+
+            RefresherValueChanged += async () =>
             {
-                StatedDecorator.SetError(new ErrorData());
+                var id = _item.Id;
+                var url = _item.Rss;
+                await _repository.StartUpdateAllByInternet(url, id);
+
+                RefreshControl.EndRefreshing();
+            };
+
+            ReloadData();
+
+            _item.PropertyChanged += (sender, args) => ReloadData();
+            _item.RssMessageModels.SubscribeForNotifications((sender, changes, error) => ReloadData());
+        }
+
+        private void ReloadData()
+        {
+            if (_item.IsValid)
+            {
+                var newItems = _rssMessagesRepository.GetMessagesForRss(_item);
+                Source.SetList(newItems);
+
+                TableView.ReloadData();
+
+                if (Source.ItemsCount > 0)
+                {
+                    StatedDecorator.SetNormal(new NormalData());
+                }
+                else
+                {
+                    StatedDecorator.SetError(new ErrorData());
+                }
             }
         }
     }
