45f758f9e8fa3a0ee0948b5a4c2594179c300af2
asura <theworldtocollapse@mail.ru>
2019-01-10 00:17:04 +0500

    format manifest

---
+++ RssClientByXamarin/Droid/Infrastructure/Locale/Locale.cs
@@ -0,0 +1,14 @@
+﻿using Shared.App.Locale;
+
+namespace RssClient.Infrastructure.Locale
+{
+    public class Locale : ILocale
+    {
+        public string GetCurrentLocaleId()
+        {
+            var androidLocale = Java.Util.Locale.Default;
+            var netLanguage = androidLocale.ToString().Replace("_", "-");
+            return netLanguage.ToLower();
+        }
+    }
+}
---
+++ RssClientByXamarin/iOS/Infrastructure/Locale/Locale.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using Shared.App.Locale;
+
+namespace iOS.Infrastructure.Locale
+{
+    public class Locale : ILocale
+    {
+        public string GetCurrentLocaleId()
+        {
+            var locale = NSLocale.CurrentLocale.CountryCode;
+            return locale.ToLower();
+        }
+    }
+}
