6e25ba5c9e550cc9ef424a38b1efbe6b5d28e1af
asura <theworldtocollapse@mail.ru>
2019-01-09 21:39:48 +0500

    delete Rss/RssEventName.cs

--- RssClientByXamarin/Shared/App/Rss/RssEventName.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Metrics
-{
-	public static class RssEventName
-	{
-		public static string CreateRssItem => "CreateRssItem";
-		public static string DeleteRssItem => "DeleteRssItem";
-		public static string EditRssItem => "EditRssItem";
-	}
-}
