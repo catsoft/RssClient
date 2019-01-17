d6a471a032c25d83826712dedc31dfc3a543b1d4
asura <theworldtocollapse@mail.ru>
2019-01-18 00:01:59 +0500

    сверстал контакты, сделал открытие

--- RssClientByXamarin/Droid/Screens/Contacts/ContactsFragment.cs
+++ RssClientByXamarin/Droid/Screens/Contacts/ContactsFragment.cs
@@ -1,6 +1,14 @@
-﻿using Android.OS;
+﻿using System;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Graphics;
+using Android.Net;
+using Android.OS;
 using Android.Views;
+using Android.Widget;
 using Droid.Screens.Navigation;
+using Xamarin.Essentials;
+using Uri = Android.Net.Uri;
 
 namespace Droid.Screens.Contacts
 {
@@ -20,7 +28,42 @@ namespace Droid.Screens.Contacts
 
             Title = Activity.GetString(Resource.String.contacts_title);
 
+            var root = view.FindViewById<LinearLayout>(Resource.Id.linearLayout_contacts_root);
+
+            InflateAndFill(inflater, root, Resource.Drawable.telegram_48, Resource.String.contacts_telegram, Resource.String.contacts_telegramLink);
+            InflateAndFill(inflater, root, Resource.Drawable.email_48, Resource.String.contacts_mail, Resource.String.contacts_mailLink);
+
             return view;
+        }
+
+        private View InflateAndFill(LayoutInflater inflater, ViewGroup container, int imageId, int titleId, int linkId)
+        {
+            var linkView = inflater.Inflate(Resource.Layout.item_link_element_contacts, container, false);
+
+            var linkText = Activity.GetText(linkId);
+            linkView.Click += (sender, args) => OpenLink(linkText);
+
+            var icon = linkView.FindViewById<ImageView>(Resource.Id.imageView_linkElementContacts_icon);
+            var title = linkView.FindViewById<TextView>(Resource.Id.textView_linkElementContacts_title);
+
+            icon.SetImageResource(imageId);
+            var text = Context.GetText(titleId);
+            title.Text = text;
+
+            container.AddView(linkView);
+
+            return linkView;
+        }
+
+        private async void OpenLink(string link)
+        {
+            if (await Launcher.CanOpenAsync(link))
+                await Launcher.OpenAsync(link);
+        }
+
+        private void OpenTelegram()
+        {
+
         }
     }
 }
