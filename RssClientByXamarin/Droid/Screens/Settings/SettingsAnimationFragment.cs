0e6680e697ae5b58dd32f389c6ff69567f3c6bb0
Alex Vakhtin <a.vakhtin@skbkontur.ru>
2019-01-29 09:58:48 +0300

    верстка под настройки анимации

--- RssClientByXamarin/Droid/Screens/Settings/SettingsAnimationFragment.cs
+++ RssClientByXamarin/Droid/Screens/Settings/SettingsAnimationFragment.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using Android.OS;
+using Android.Support.V7.Widget;
 using Android.Views;
+using Android.Widget;
 using Droid.Screens.Navigation;
 
 namespace Droid.Screens.Settings
@@ -21,6 +24,28 @@ namespace Droid.Screens.Settings
         {
             var view = base.OnCreateView(inflater, container, savedInstanceState);
 
+            var speedSpinner = view.FindViewById<AppCompatSpinner>(Resource.Id.appCompatSpinner_settingsStartPage_speedAnimation);
+            var typeSpinner = view.FindViewById<AppCompatSpinner>(Resource.Id.appCompatSpinner_settingsStartPage_typeAnimation);
+            
+            speedSpinner.Adapter = new ArrayAdapter(Context, Resource.Layout.support_simple_spinner_dropdown_item, new List<string>()
+            {
+                "0.25x",
+                "0.5x",
+                "1x",
+                "2x",
+                "4x",
+                "8x"
+            });
+            
+            typeSpinner.Adapter = new ArrayAdapter(Context, Resource.Layout.support_simple_spinner_dropdown_item, new List<string>()
+            {
+                "None",
+                "Fade",
+                "From left",
+                "From right",
+                "From bottom"
+            });
+            
             return view;
         }
     }
