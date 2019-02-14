fdb4bac9ed1e39a816b00c86398bbdda493e1973
Alex Vakhtin <a.vakhtin@skbkontur.ru>
2019-02-14 12:44:13 +0300

    fix castE

--- RssClientByXamarin/Droid/Screens/RssAllMessagesFilter/Filter/RssAllMessagesFilterSubFragment.cs
+++ RssClientByXamarin/Droid/Screens/RssAllMessagesFilter/Filter/RssAllMessagesFilterSubFragment.cs
@@ -1,5 +1,7 @@
 ﻿using Android.OS;
 using Android.Views;
+using Android.Widget;
+using AndroidX.Browser.BrowserActions;
 using Droid.Container;
 using Droid.Repository;
 using Droid.Screens.Navigation;
@@ -27,7 +29,39 @@ namespace Droid.Screens.RssAllMessagesFilter.Filter
         {
             var view = base.OnCreateView(inflater, container, savedInstanceState);
 
-            var filter = _configurationRepository.GetSettings<AllMessageFilterConfiguration>();
+            var filterConfiguration = _configurationRepository.GetSettings<AllMessageFilterConfiguration>();
+
+            var favoriteCheckBox = view.FindViewById<CheckBox>(Resource.Id.checkBox_AllMessagesFilter_favorite);
+            var readCheckBox = view.FindViewById<CheckBox>(Resource.Id.checkBox_AllMessagesFilter_read);
+            var unreadCheckBox = view.FindViewById<CheckBox>(Resource.Id.checkBox_AllMessagesFilter_unread);
+            // TODO добавить исчо дату
+            var fromButton = view.FindViewById<Button>(Resource.Id.button_AllMessagesFilter_dateFrom);
+            var toButton= view.FindViewById<Button>(Resource.Id.button_AllMessagesFilter_dateTo);
+
+            favoriteCheckBox.Checked = filterConfiguration.IsFavorite;
+            readCheckBox.Checked = filterConfiguration.IsRead;
+            unreadCheckBox.Checked = filterConfiguration.IsUnread;
+
+            favoriteCheckBox.CheckedChange += (sender, args) =>
+            {
+                var filter = _configurationRepository.GetSettings<AllMessageFilterConfiguration>();
+                filter.IsFavorite = args.IsChecked;
+                _configurationRepository.SaveSetting(filter);
+            };
+            
+            readCheckBox.CheckedChange += (sender, args) =>
+            {
+                var filter = _configurationRepository.GetSettings<AllMessageFilterConfiguration>();
+                filter.IsRead = args.IsChecked;
+                _configurationRepository.SaveSetting(filter);
+            };
+            
+            unreadCheckBox.CheckedChange += (sender, args) =>
+            {
+                var filter = _configurationRepository.GetSettings<AllMessageFilterConfiguration>();
+                filter.IsUnread = args.IsChecked;
+                _configurationRepository.SaveSetting(filter);
+            };
             
             return view;
         }
