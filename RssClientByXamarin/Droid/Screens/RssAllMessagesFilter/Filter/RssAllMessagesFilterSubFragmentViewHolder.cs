6642ea5f8734a076daaf427845412b20189380b0
asura <theworldtocollapse@mail.ru>
2019-04-18 03:18:53 +0300

    filter subfragment

---
+++ RssClientByXamarin/Droid/Screens/RssAllMessagesFilter/Filter/RssAllMessagesFilterSubFragmentViewHolder.cs
@@ -0,0 +1,34 @@
+using Android.Views;
+using Android.Widget;
+
+namespace Droid.Screens.RssAllMessagesFilter.Filter
+{
+    public class RssAllMessagesFilterSubFragmentViewHolder
+    {
+        public RssAllMessagesFilterSubFragmentViewHolder(View view)
+        {
+            RootRadioGroup = view.FindViewById<RadioGroup>(Resource.Id.radioGroup_rss_all_messages_filter_main);
+            AllRadioButton = view.FindViewById<RadioButton>(Resource.Id.radioButton_rss_all_messages_filter_all);
+            FavoriteRadioButton = view.FindViewById<RadioButton>(Resource.Id.radioButton_rss_all_messages_filter_favorite);
+            ReadRadioButton = view.FindViewById<RadioButton>(Resource.Id.radioButton_rss_all_messages_filter_read);
+            UnreadRadioButton = view.FindViewById<RadioButton>(Resource.Id.radioButton_rss_all_messages_filter_unread);
+            
+            FromButton = view.FindViewById<Button>(Resource.Id.button_AllMessagesFilter_dateFrom);
+            ToButton = view.FindViewById<Button>(Resource.Id.button_AllMessagesFilter_dateTo);
+        }
+
+        public RadioGroup RootRadioGroup { get; }
+        
+        public RadioButton AllRadioButton { get; }
+
+        public RadioButton FavoriteRadioButton { get; }
+
+        public RadioButton ReadRadioButton { get; }
+
+        public RadioButton UnreadRadioButton { get; }
+        
+        public Button FromButton { get; }
+        
+        public Button ToButton { get; }
+    }
+}
