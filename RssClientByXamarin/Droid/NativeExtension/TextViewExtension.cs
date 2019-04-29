e2377fa6d7515489f3fd5f2fdd528322f6b9f8f9
Asura <asuraprog@gmail.com>
2019-04-29 16:35:46 +0300

    set text problem from play market

--- RssClientByXamarin/Droid/NativeExtension/TextViewExtension.cs
+++ RssClientByXamarin/Droid/NativeExtension/TextViewExtension.cs
@@ -9,6 +9,8 @@ namespace Droid.NativeExtension
     {
         public static void SetTextAsHtml([CanBeNull] this TextView textView, [CanBeNull] string text)
         {
+            if (string.IsNullOrEmpty(text)) return;
+            
             var spanned = Build.VERSION.SdkInt >= BuildVersionCodes.N
                 ? Html.FromHtml(text, FromHtmlOptions.ModeLegacy)
                 // TODO Разобраться с сеттером текста
